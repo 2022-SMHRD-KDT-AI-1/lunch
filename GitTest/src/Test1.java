2118bbcd4cccbc2801a5eba2063d843f153e132a
smhrd <smhrd@218.149.140.100>
2022-01-15 14:52:40 +0900

    test Commit

--- GitTest/src/Test1.java
+++ GitTest/src/Test1.java
@@ -3,9 +3,11 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("첫번째 커밋 ");
+		System.out.println("泥ル쾲吏� 而ㅻ컠 ");
 		
-		System.out.println("두번째 커밋");
+		System.out.println("�몢踰덉㎏ 而ㅻ컠");
+		
+		System.out.println("test");
 	}
 
-}
+} 
